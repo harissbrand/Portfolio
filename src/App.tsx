1b48a09a8ac3ec5d16ce829679bc3cc11387764e
ShinEi4 <harisbrandon44@gmail.com>
2026-09-07 20:19:03 +0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <h1 className="text-5xl font-bold">
+        My Portfolio
+      </h1>
+    </div>
+  )
+}
+
+export default App
